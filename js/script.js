9722410d50e481d332f8ea97eb4281b7fadb72d8
市川ひなた <hinakami0701@gmail.com>
2018-02-07 02:03:21 +0900

    ホームの左ナビゲーションの作成途中

--- js/script.js
+++ js/script.js
@@ -1,22 +1,21 @@
 $(function() {
  
-  // //マウスを乗せたら発動
-  // $('.header-nav-right i').hover(function() {
+ 	//マウスを乗せたら発動
+ 	$('.header-nav-right i').hover(function() {
 
-  // 	var index = $('.header-nav-right i').index(this);
+  	// var index = $('.header-nav-right i').index(this);
+
+  	function Navhover(){
+ 		// $('nav-detail' + index).addClass("nav-detail-hovered");
+ 		$(this).balloon();
+ 	}
  	
- 	// setTimeout("header_nav_hover",500);
+ 	setTimeout(Navhover,500);
 
- 	// function header_nav_hover(){
- 	// 	$('nav-detail' + index).addClass("nav-detail-hovered");
- 	// }
-  //   //マウスを乗せたら色が変わる
-  //   $(this).css('background', '#c00');
- 
-  // //ここにはマウスを離したときの動作を記述
-  // }, function() {
- 
-  //   $('nav-detail' + index).removeClass("nav-detail-hovered");
- 
-  // });
+	//ここにはマウスを離したときの動作を記述
+	}, function() {
+
+	// $('nav-detail' + index).removeClass("nav-detail-hovered");
+
+	});
 });
