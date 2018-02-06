abfe4c966b6f9f06ee0b99369d611475cd22cfec
市川ひなた <hinakami0701@gmail.com>
2018-02-06 15:32:42 +0900

    navの詳細表示をつける途中

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(function() {
+ 
+  // //マウスを乗せたら発動
+  // $('.header-nav-right i').hover(function() {
+
+  // 	var index = $('.header-nav-right i').index(this);
+ 	
+ 	// setTimeout("header_nav_hover",500);
+
+ 	// function header_nav_hover(){
+ 	// 	$('nav-detail' + index).addClass("nav-detail-hovered");
+ 	// }
+  //   //マウスを乗せたら色が変わる
+  //   $(this).css('background', '#c00');
+ 
+  // //ここにはマウスを離したときの動作を記述
+  // }, function() {
+ 
+  //   $('nav-detail' + index).removeClass("nav-detail-hovered");
+ 
+  // });
+});
